233a8cfbcbbbdb676b573c4f4f9abb612c412026
Anderson <andyvini.developer@gmail.com>
2024-03-08 20:49:25 +0100

    primer commit, y creacion de la bd y la tabla citas

---
+++ database/migrations/2024_03_08_192914_create_table_citas.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('table_citas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre_cliente');
+            $table->string('telefono');
+            $table->date('fecha');
+            $table->time('hora');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('table_citas');
+    }
+};
